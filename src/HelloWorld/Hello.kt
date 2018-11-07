84a076c68f739e52158ccaa6509e787af37e28c3
George Jacob <georgekunchattil@gmail.com>
2018-11-07 18:01:01 +0530

    print name, do and prnt arithmetic operations, print boolean values

--- src/HelloWorld/Hello.kt
+++ src/HelloWorld/Hello.kt
@@ -1,5 +1,13 @@
 package HelloWorld
 
 fun main (args: Array<String>) : Unit {
-    print("Hello, World")
+    //print name
+    println("Hello, World")
+    println("Hello Kotilin")
+
+    //arthimetic operation
+    println(10.0 / 9)
+
+    //print boolean values
+    println(true)
 }
